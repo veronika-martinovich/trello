19f6820787f31707802564c44c9387c606c8bd53
Veronika Martinovich <veranika.mart@gmail.com>
2020-10-07 13:22:09 +0300

    feat: create user.memory.repository methods

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,36 @@
+const DB = require('../../utils/inMemoryDb');
+const TABLE_NAME = 'Users';
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return DB.getAllEntities(TABLE_NAME);
 };
 
-module.exports = { getAll };
+const get = async id => {
+  const entity = await DB.getEntity(TABLE_NAME, id);
+  if (!entity) {
+    throw new Error(`Couldn't find user with id: ${id}`);
+  }
+  return entity;
+};
+
+const update = async (id, user) => {
+  const entity = await DB.updateEntity(TABLE_NAME, id, user);
+  if (!entity) {
+    throw new Error(`Couldn't find user with id: ${id}`);
+  }
+  return entity;
+};
+
+const remove = async id => {
+  const entity = DB.removeEntity(TABLE_NAME, id);
+  if (!entity) {
+    throw new Error(`Couldn't find user with id: ${id}`);
+  }
+  return entity;
+};
+
+const save = async user => {
+  return DB.saveEntity(TABLE_NAME, user);
+};
+
+module.exports = { getAll, get, update, remove, save };
