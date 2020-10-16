42dccc56aa885dab9aa1fc9e1deb25524f32524b
Veronika Martinovich <veranika.mart@gmail.com>
2020-10-16 14:22:50 +0300

    feat: add validation and error handling for users

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,6 +1,7 @@
 const usersRouter = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
+const isUserValid = require('./user.validation');
 const asyncErrorHandler = require('../../helpers/errorHandlers')
   .asyncErrorHandler;
 
@@ -8,22 +9,18 @@ usersRouter
   .route('/')
   .get(
     asyncErrorHandler(async (req, res) => {
-      try {
-        const users = await usersService.getAll();
-        res.json(users.map(user => User.toResponse(user)));
-      } catch (error) {
-        res.status(404).send('404 Not Found');
-      }
+      // reject(Error('Oops!'))
+      const users = await usersService.getAll();
+      res.json(users.map(user => User.toResponse(user)));
     })
   )
   .post(
     asyncErrorHandler(async (req, res) => {
-      try {
-        const user = await usersService.save(User.createFromRequest(req.body));
-        res.status(200).send(User.toResponse(user));
-      } catch (error) {
-        res.status(404).send('404 Not Found');
-      }
+      // reject(Error('Oops!'))
+      const userToCreate = User.createFromRequest(req.body);
+      isUserValid(userToCreate);
+      const user = await usersService.save(userToCreate);
+      res.status(200).send(User.toResponse(user));
     })
   );
 
@@ -31,32 +28,23 @@ usersRouter
   .route('/:userId')
   .get(
     asyncErrorHandler(async (req, res) => {
-      try {
-        const user = await usersService.get(req.params.userId);
-        res.status(200).send(User.toResponse(user));
-      } catch (error) {
-        res.status(404).send('404 Not Found');
-      }
+      // reject(Error('Oops!'))
+      const user = await usersService.get(req.params.userId);
+      res.status(200).send(User.toResponse(user));
     })
   )
   .delete(
     asyncErrorHandler(async (req, res) => {
-      try {
-        await usersService.remove(req.params.userId);
-        res.sendStatus(204);
-      } catch (error) {
-        res.status(404).send('404 Not Found');
-      }
+      // reject(Error('Oops!'))
+      await usersService.remove(req.params.userId);
+      res.sendStatus(204);
     })
   )
   .put(
     asyncErrorHandler(async (req, res) => {
-      try {
-        const user = await usersService.update(req.params.userId, req.body);
-        res.status(200).send(User.toResponse(user));
-      } catch (error) {
-        res.status(404).send('404 Not Found');
-      }
+      // reject(Error('Oops!'))
+      const user = await usersService.update(req.params.userId, req.body);
+      res.status(200).send(User.toResponse(user));
     })
   );
 
